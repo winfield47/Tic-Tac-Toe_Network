6c9bd8b9b578175aded1639c494479523a2b3d71
Winfield202 <99300401+Winfield202@users.noreply.github.com>
2023-12-04 14:07:14 -0800

    client now sends move request instead of game data

--- client.py
+++ client.py
@@ -19,7 +19,7 @@ def displayWhoWon(gameData):
         print("ERROR")
 
 
-def convertSpotValue(spot):
+def convertSpotValue(spot, count):
     if spot == 0:
         return " "
     if spot == 1:
@@ -34,29 +34,18 @@ def printGameBoard(spots):
     print("+---+---+---+")
     count = 0
     for spot in spots:
-        print("| " + convertSpotValue(spot), end=" ")
+        print("| " + convertSpotValue(spot, count), end=" ")
         count += 1
         if count % 3 == 0:
             print("|\n+---+---+---+")
 
 
-def takeTurn(gameData):
-    # Take a Turn!
-    print("Please enter the position you'd like to place an 'O'\n(left to right, then top to bottom):")
-    print("Ex: 1 -> top left")
-    print("Ex: 5 -> middle")
-    print("Ex: 9 -> bottom right")
-
+def getTurn():
     while True:
         try:
             choice = int(input("Choice: "))
             if 1 <= choice <= 9:
-                if gameData[3 + choice] == 0:
-                    gameData[3 + choice] = 1
-                    gameData[3] = 1
-                    break
-                else:
-                    print("Spot already occupied. Please choose an empty spot.")
+                return choice
             else:
                 print("Invalid choice. Please choose a number between 1 and 9.")
         except ValueError:
@@ -73,50 +62,58 @@ def main():
     target_port = 10101
 
     # TicTacToe
-    sendCount = 0
     continueKey = 'y'
     try:
+
         # create a socket object
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
         # connect the client
         client_socket.connect((target_host, target_port))
+        print(f"[*] Connected to {target_host}:{target_port}")
 
         while continueKey.lower() == 'y' or continueKey == '':
-            gameData = [0, 13, sendCount, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+            # base data for gameData
+            gameData = [0, 13, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
             while True:
 
-                if sendCount == 0:
-                    print(f"[*] Connected to {target_host}:{target_port}")
+                # initialize the data i need to send
+                request = [2, 3, 0]  # 0 is a placeholder
 
+                # server updating our game type
                 if gameData[0] == 0:
 
                     if gameData[1] == 13:
 
                         # show the board
-                        printGameBoard(gameData[4:])
+                        if gameData[3] != 1:
+                            printGameBoard(gameData[4:])
+                            # Take a Turn!
+                            print("Please enter the position you'd like to place your piece.")
+                            print("(left to right, then top to bottom):")
+                            print("Ex: 1 -> top left")
+                            print("Ex: 5 -> middle")
+                            print("Ex: 9 -> bottom right")
+                        else:
+                            print("That spot is taken!")
 
                         # take your turn
-                        takeTurn(gameData)
-
-                        # display turn taken
-                        printGameBoard(gameData[4:])
+                        request[2] = getTurn()
 
                         # convert data to a format that can be sent through a socket
-                        gameData[2] += 1
-                        gameDataStr = ','.join(map(str, gameData))
+                        request = ','.join(map(str, request))
 
                         # send the data
-                        client_socket.send(gameDataStr.encode())
+                        client_socket.send(request.encode())
 
                 # receive some data
-                response = client_socket.recv(4096)
+                server_response = client_socket.recv(4096)
 
                 # print what was sent to us from the server we connected to
-                response = response.decode()
-                serverGameData = list(map(int, response.split(',')))
-                if serverGameData[2] == gameData[2] + 1:
+                server_response = server_response.decode()
+                serverGameData = list(map(int, server_response.split(',')))
+                if serverGameData[2] > gameData[2]:
                     gameData = serverGameData
 
                 # if the game is over
@@ -125,8 +122,8 @@ def main():
                     break
 
             displayWhoWon(gameData)
+            printGameBoard(gameData[4:])
 
-            sendCount = gameData[2]
             print("Game over!")
             continueKey = input("Continue playing? (Y/n)?: ")
 
@@ -142,8 +139,7 @@ def main():
         print("\n[*] Exiting...")
     except ConnectionRefusedError as cre:
         print("ConnectionRefusedError: ", format(cre.args[0]))
-        if sendCount == 0:
-            print("It's possible the IPv4 Address you entered is incorrect.\nTry using: 127.0.0.1")
+        print("It's possible the IPv4 Address you entered is incorrect.\nTry using: 127.0.0.1")
 
 
 if __name__ == "__main__":
